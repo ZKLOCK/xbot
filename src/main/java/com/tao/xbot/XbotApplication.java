aacbfb1125df3aab9aa992e89aaec5915677139b
ZKLOCK <303132376@qq.com>
2026-01-29 21:47:49 +0800

    Add Maven wrapper configuration for Java upgrade

---
+++ src/main/java/com/tao/xbot/XbotApplication.java
@@ -0,0 +1,14 @@
+package com.tao.xbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbotApplication.class, args);
+		System.out.println("XbotApplication started");
+	}
+
+}
---
+++ src/test/java/com/tao/xbot/XbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tao.xbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
