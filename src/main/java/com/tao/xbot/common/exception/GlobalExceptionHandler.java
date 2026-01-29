8142061f20181c0fb335359871d64e965e53b9fb
ZKLOCK <303132376@qq.com>
2026-01-30 00:49:38 +0800

    Remove duplicate Order.java and VLTest.java - cleanup and prepare for deployment

---
+++ src/main/java/com/tao/xbot/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.tao.xbot.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tao.xbot.common.util.Result;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(BusinessException.class)
+    public Result<Void> handleBusinessException(BusinessException e) {
+        log.error("业务异常: {}", e.getMessage());
+        return Result.error(e.getMessage());
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception e) {
+        log.error("系统异常", e);
+        return Result.error("系统异常，请稍后重试");
+    }
+}
